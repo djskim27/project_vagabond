6314e487df834b73c4ca260778af8432eef15b1c
djskim27 <djs.kim27@gmail.com>
2017-08-30 15:52:02 -0400

    got edit form to work for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
  
   def show
-    @post = Post.find(params[:id])
+    @city = City.find(params[:city_id])
+    @post = @city.posts.find(params[:id])
   end
 
   def new
@@ -17,8 +18,18 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    @city = City.find(params[:city_id])
+    @post = @city.posts.find(params[:id])
+   
   end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update post_params 
+
+    redirect_to city_post_path(@post)
+  end
+
 
   private
   def post_params
