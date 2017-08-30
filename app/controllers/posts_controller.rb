2769f56403f933dbd81a8ec3807d943f2c676853
djskim27 <djs.kim27@gmail.com>
2017-08-30 15:09:30 -0400

    finished new post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,23 @@ class PostsController < ApplicationController
   end
 
   def new
+    @city = City.find(params[:city_id])
     @post = Post.new
+  end
+
+  def create
+    @city = City.find(params[:city_id])
+    @post = @city.posts.create!(post_params)
+
+    redirect_to city_path(@city)
   end
 
   def edit
     @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :city_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-
-  get 'posts/show'
-
-  get 'posts/edit'
-
+ 
   root to: 'cities#index'
-  resources :cities
-  resources :posts
+  resources :cities do
+    resources :posts
+  end
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
