f752cd81cd0df54d76a35e6223ae17c574783676
djskim27 <djs.kim27@gmail.com>
2017-08-30 13:58:14 -0400

    added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 City.destroy_all
-Post.destroy_all
+
 
 atlanta = City.create(name: 'Atlanta', country: "United States", img_url: "http://www.marketoneinstitutional.com/wp-content/uploads/2015/04/M1-background.jpg")
 sanfrancisco = City.create(name: 'San Francisco', country: "United States", img_url: "http://www.twitrcovers.com/wp-content/uploads/2013/12/Oakland-Bay-San-Francisco-l.jpg")
 london = City.create(name: 'London', country: "United Kingdom", img_url: "http://www.centrallondonrents.com/wp-content/uploads/2015/02/london_background.jpg")
+losangeles = City.create(name: 'Los Angeles', country: "United States", img_url: "http://pacpet.com/wp-content/uploads/2016/10/Los-Angeles-background.jpg")
+sydney = City.create(name: 'Sydney', country: "Australia", img_url: "http://www.remotetraveler.com/wp-content/gallery/sydney-harbour-bridge/Sydney-Harbour-Bridge-in-the-background.jpg")
 
 atl_post_1 = Post.create(title: 'Met Some Dude Named Dirty Mike', content: 'He would not stop talking about his lizards and tropical frogs.')
 atlanta.posts = [
