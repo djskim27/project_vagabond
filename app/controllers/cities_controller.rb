3e3d90e4d7e72bc367ea3e1dbd8e2cba65cfe711
djskim27 <djs.kim27@gmail.com>
2017-08-31 17:43:45 -0400

    finished profile page

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -6,7 +6,7 @@ class CitiesController < ApplicationController
   def show
     @city = City.friendly.find params[:id]
     @city_posts = @city.posts.paginate(page: params[:page], per_page: 10).order('created_at DESC') 
-    
+    @user = current_user
     
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
  
+  get 'users/show'
+
   devise_for :users
   root to: 'cities#index'
   resources :cities do
