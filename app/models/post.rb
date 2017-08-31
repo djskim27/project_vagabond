951f659ae82cbcf646257f2484c407696d01e920
djskim27 <djs.kim27@gmail.com>
2017-08-31 15:58:56 -0400

    got devise to work/ can sign up or log in as user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :city
+  belongs_to :user
   validates :title, presence: true, length: {maximum:200, too_long:"must have a maximum of 200 characters!"}
   validates :content, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  
+  devise_for :users
   root to: 'cities#index'
   resources :cities do
     resources :posts
